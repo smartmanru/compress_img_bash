d973e370a75e147ca3491dc863ade3c361b49757
root <root@v165555.hosted-by-vdsina.ru>
2020-04-15 13:16:45 +0300

    experemental

---
+++ comress_folder1.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+cd `dirname $0`
+h=`pwd`
+echo $bash
+
+export ptp=$(pwd)
+subfo=($(ls -d ${ptp}*/*/))
+for ((i=0;i<${#subfo[@]};i++)); do
+    d=${subfo[$i]}
+    cd $d
+export f=$(basename $PWD)
+    cd -
+ 
+
+h_len=$(echo `expr "$h" : '.*'`)
+
+#*.{jpg,png,gif,jpeg}
+function check {
+#export	count=$($pn |wc -l)
+
+#count=$($pn|wc -l)
+#sleep 10
+#old_count=$count
+tmp=tmp
+mkdir -p $tmp
+#count=$($pn |wc -l)
+#if [[ "$old_count" != "$count" ]];
+#	then ((c=$count-$old_count))
+	
+		for r in $(find $f|grep jpg);
+		do
+			echo $r
+			e=$(echo "${r##*.}")
+			n=`basename -- "$r"`
+			echo $e
+				if [[ "$e" == "jpg" || "$e" == "png" || "$e" == "gif" ]]
+				then
+				fp=$(echo "$r"|awk -F "$n" {'print $1'})
+				pa=${r:$h_len}
+				r_len=$(echo `expr "$r" : '.*'`) #длина пути полного
+				n_len=$(echo `expr "$n" : '.*'`) #длина имени 
+				pa_len=$(echo `expr "$pa" : '.*'`)
+
+				len_np=$(expr $pa_len-$n_len)
+				np=${pa:0:$len_np}
+				#echo "/opt/resmushit-cli.sh --preserve-exif -notime -q 50 -o /fiche/images --preserve-n /tmp/$r" 
+		
+				./resmushit -q 50 "$r"  --preserve-exif --preserve-filename -o tmp/$np
+				if [ $? != 0 ] 
+				then 
+					./resmushit -q 50 "$r"  --preserve-exif --preserve-filename -o tmp/$np
+				fi
+				# if [[ ! -f "$fp/$r" ]]
+				# then
+				# 		mv {./$tmp,$fp}/$r
+				# fi
+			fi
+				
+		done
+
+	}
+
+check
+#mv  -f $tmp/*  $f/
+done
